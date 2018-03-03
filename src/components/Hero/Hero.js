ffbd62319ec6cdd17a692c738a1b204304b3a844
Joao Matheus <joao.matheus2819@gmail>
2018-03-03 14:18:57 -0300

    Scss button create

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import Header from '../Header/Header';
+import Button from '../Button';
+import Header from '../Header';
+import DownloadIcon, { AndroidIcon } from './../Icon';
 import './styles/Hero.scss';
 
-class Hero extends Component{
-    render(){
-        return(
-            <section className='Hero'>
-              <Header/>
-            </section>
-        )
-    }
+class Hero extends Component {
+  render() {
+    return (
+      <section className='Hero'>
+        <Header />
+
+        <Button className='default'>
+          <DownloadIcon fill='#FFF' /> Download
+        </Button>
+
+        <Button>LEARN MORE</Button>
+
+        <Button className="about">
+          <AndroidIcon fill="#FFF" />
+          Available on the <br /> Google Play
+        </Button>
+
+      </section>
+    )
+  }
 }
 
 export default Hero
