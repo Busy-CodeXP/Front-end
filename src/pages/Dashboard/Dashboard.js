e29981d7eebbb5482460defe8c5e0518664f7737
GuiGalli <guilhermegalli5@hotmail.com>
2018-03-06 18:51:12 -0300

    create Dashboard structure page

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import './styles/Dashboard.scss';
+
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <Fragment>
+        
+      </Fragment>
+    );
+  }
+}
+
+export default Dashboard;
