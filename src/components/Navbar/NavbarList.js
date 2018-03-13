b3834956ba2bb51dc6eaafbb86556e16b02b9f0e
GuiGalli <guilhermegalli5@hotmail.com>
2018-03-13 19:53:34 -0300

    refactoring NavbarList for change className

--- src/components/Navbar/NavbarList.js
+++ src/components/Navbar/NavbarList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-const NavbarList = ({ children, link }) => {
+const NavbarList = ({ children, link, className }) => {
   if (link) {
     return (
-      <li>
+      <li className={className}>
         <Link to={link}>
           {children}
         </Link>
@@ -12,7 +12,7 @@ const NavbarList = ({ children, link }) => {
     );
   } else {
     return (
-      <li>
+      <li className={className}>
         {children}
       </li>
     );
