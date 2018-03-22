e3c6066fc2e4ffabff621753784f2cd53e0955b6
João Matheus <joao.matheus2819@gmail.com>
2018-03-21 21:03:01 -0300

    refactor search

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 
 
 import { NavbarList } from '../../components/Navbar';
@@ -7,20 +7,20 @@ import BoxInfo from '../../components/BoxInfo/BoxInfo';
 import Header from '../../components/Header';
 import BusInfo from '../../components/BusInfo/BusInfo';
 import GoogleMaps from '../../components/GoogleMaps/GoogleMaps';
-import SearchField from './../../components/SearchField/SearchField';
 import './styles/Dashboard.scss';
 
 
 class Dashboard extends Component {
   state = {
-    list: []
+    inputText: '',
+    results: []
   }
 
-  getBus(seila) {
-    return Axios.get(`https://swapi.co/api/people/${seila}`)
-      .then((response) => {
+  getBus = () => {
+    return axios.get(`https://swapi.co/api/people/${this.state.inputText}`)
+      .then(({ data }) => {
         this.setState({
-          list: response.data.films
+          results: data.films
         })
       })
       .catch((error) => {
@@ -28,24 +28,28 @@ class Dashboard extends Component {
       })
   }
 
-  componentWillUpdate(nextProps, nextState) {
-    // console.log(nextState)
+  handleInputChange = () => {
+    this.setState({
+      inputText: this.search.value
+    }, () => {
+      if (this.state.inputText.length > 1) {
+        this.getBus()
+      } else {
+        this.setState({
+          results: []
+        })
+      }
+    })
   }
-  teste = (aa) => {
-    if (aa.length >= 1) {
-      this.getBus(aa)
-    }
-  }
-
   render() {
-    console.log(this.state)
     const nav = <svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 485.213 485.213">
       <g>
         <path d="M363.909,181.955C363.909,81.473,282.44,0,181.956,0C81.474,0,0.001,81.473,0.001,181.955s81.473,181.951,181.955,181.951    C282.44,363.906,363.909,282.437,363.909,181.955z M181.956,318.416c-75.252,0-136.465-61.208-136.465-136.46    c0-75.252,61.213-136.465,136.465-136.465c75.25,0,136.468,61.213,136.468,136.465    C318.424,257.208,257.206,318.416,181.956,318.416z" fill="#FFFFFF" />
         <path d="M471.882,407.567L360.567,296.243c-16.586,25.795-38.536,47.734-64.331,64.321l111.324,111.324    c17.772,17.768,46.587,17.768,64.321,0C489.654,454.149,489.654,425.334,471.882,407.567z" fill="#FFFFFF" />
       </g>
     </svg>
-    const items = this.state.list && this.state.list.map(
+
+    const items = this.state.results && this.state.results.map(
       item =>
         <NavbarList key={item} className='list-item-bus'>
           <p>{item}</p>
@@ -54,13 +58,16 @@ class Dashboard extends Component {
     return (
       <Fragment>
         <Header background='#0893C3' burger={nav}>
-          <SearchField funcao={this.teste} />
+          <input
+            placeholder="procura ae"
+            ref={input => this.search = input}
+            onChange={this.handleInputChange}
+          />
           {items}
         </Header>
 
         <BusInfo
           linha='1234-10'
-          trajeto='TERM. JD. BOLADO / JD. OSASCO'
         />
 
         <section className='Dashboard-BoxInfo'>
