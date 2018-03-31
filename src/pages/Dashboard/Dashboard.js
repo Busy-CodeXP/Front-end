ec669ebf59021aa247a447934d4783ced2372478
Joao Matheus <joao.matheus2819@gmail>
2018-03-30 21:15:13 -0300

    mock

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import * as BusyApi from "../../api/BusyApi";
+import * as mockdados from "./mockdados";
 
 import { NavbarList } from '../../components/Navbar';
 import BoxInfo from '../../components/BoxInfo/BoxInfo';
@@ -18,27 +19,33 @@ class Dashboard extends Component {
   }
 
   getLinha = () => {
-    BusyApi.getLinhas(this.state.inputText)
-      .then(({ data }) => {
-        this.setState({
-          results: data
-        })
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+    this.setState({
+      results: mockdados.linha8000
+    })
+    // BusyApi.getLinhas(this.state.inputText)
+    //   .then(({ data }) => {
+    //     this.setState({
+    //       results: data
+    //     })
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   })
   }
 
   getBus = () => {
-    BusyApi.getOnibusLinha(this.state.inputText)
-      .then(({ data }) => {
-        this.setState({
-          bus: data
-        })
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+    this.setState({
+      bus: mockdados.busLinha
+    })
+    // BusyApi.getOnibusLinha(this.state.inputText)
+    //   .then(({ data }) => {
+    //     this.setState({
+    //       bus: data
+    //     })
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   })
   }
 
   handleInputChange = () => {
@@ -66,8 +73,9 @@ class Dashboard extends Component {
       this.getBus()
     })
   }
-
+  
   render() {
+    console.log(this.state)
     const nav = <svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 485.213 485.213">
       <g>
         <path d="M363.909,181.955C363.909,81.473,282.44,0,181.956,0C81.474,0,0.001,81.473,0.001,181.955s81.473,181.951,181.955,181.951    C282.44,363.906,363.909,282.437,363.909,181.955z M181.956,318.416c-75.252,0-136.465-61.208-136.465-136.46    c0-75.252,61.213-136.465,136.465-136.465c75.25,0,136.468,61.213,136.468,136.465    C318.424,257.208,257.206,318.416,181.956,318.416z" fill="#FFFFFF" />
@@ -75,9 +83,9 @@ class Dashboard extends Component {
       </g>
     </svg>
 
-    const items = this.state.results && this.state.results.map(
-      (item, index) =>
-        <NavbarList key={index} className='list-item-bus' >
+const items = this.state.results && this.state.results.map(
+  (item, index) =>
+  <NavbarList key={index} className='list-item-bus' >
           <div onClick={() => this.handleClick(item.cl)}>
             <span>{item.lt}-{item.tl} -> {item.sl === 1 ? `${item.tp}` : `${item.ts}`}</span>
             <br />
---
+++ src/pages/Dashboard/mockdados.js
@@ -0,0 +1,64 @@
+export const linha8000 = [
+    {
+        cl: 2506,
+        lc: false,
+        lt: '8000',
+        sl: 1,
+        tl: 1,
+        tp: 'PÇA. RAMOS DE AZEVEDO',
+        ts: 'TERM. LAPA'
+    },
+    {
+        cl: 35274,
+        lc: false,
+        lt: '8000',
+        sl: 2,
+        tl: 1,
+        tp: 'PÇA. RAMOS DE AZEVEDO',
+        ts: 'TERM. LAPA'
+    },
+    {
+        cl: 1273,
+        lc: false,
+        lt: '8000',
+        sl: 1,
+        tl: 10,
+        tp: 'PÇA. RAMOS DE AZEVEDO',
+        ts: 'TERM. LAPA'
+    },
+    {
+        cl: 34041,
+        lc: false,
+        lt: '8000',
+        sl: 2,
+        tl: 10,
+        tp: 'PÇA. RAMOS DE AZEVEDO',
+        ts: 'TERM. LAPA'
+    }
+]
+
+export const busLinha = {
+    hr: '19:57',
+    vs: [
+      {
+        p: '11433',
+        a: false,
+        ta: '2017-05-07T22:57:02Z',
+        py: -23.540150375000003,
+        px: -46.64414075,
+        l: '50%',
+        empe: 10,
+        sentado: 70
+      },
+      {
+        p: '10132',
+        a: true,
+        ta: '2017-05-07T22:57:02Z',
+        py: -33.540150375,
+        px: -16.64414075,
+        l: '40%',
+        empe: 20,
+        sentado: 50
+      }
+    ]
+  }
